ccb66687300f19b88a41428be52b889d512a0bed
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-09-02 13:20:59 +0700

    Add files via upload

---
+++ kode.4.persegi.py
@@ -0,0 +1,10 @@
+# Menginput Sisi Persegi
+sisi = float(input('Tulis Sisi Persegi: '))
+
+# Hitung Luas dan Keliling persegi
+luas = sisi * sisi
+keliling = 4 * sisi
+
+#Menampilkan Hasil Perhitungan
+print('Luas Persegi adalah %0.2f' %luas)
+print('keliling Persegi adalah %0.2f' %keliling)
